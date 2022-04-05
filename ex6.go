033c75cb7e4a309d2b35e646899c955f4a9bf69a
laura <arual.socram@gmail.com>
2022-04-05 18:14:57 +0200

    day2

---
+++ ex6.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 4
+	fmt.Printf("%d\t\t%b\n", a, a) //4    100
+	// \t imprimirá una tabulació i %b mostrara en binari
+	//Hem de imprimir un binari amb una posició més cap a la dreta
+}
+/* Birshifting és quan es desplacen
+nombres binaris tant cap a la dreta o a l’esquerra un nombre determinat de
+cops. */
