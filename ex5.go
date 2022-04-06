85bb93036a7fe6c8bcd838040c5453d2203bf26a
laura <arual.socram@gmail.com>
2022-04-06 17:40:53 +0200

    day3

--- ex5.go
+++ ex5.go
@@ -9,7 +9,7 @@ type any int
 var anyActual any
 
 func main() {
-	anyActual = 2021
-	fmt.Println(anyActual)      //2021
+	anyActual = 2022
+	fmt.Println(anyActual)      //2022
 	fmt.Printf("%T", anyActual) //main.any
 }
